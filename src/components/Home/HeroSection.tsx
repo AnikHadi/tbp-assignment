0c8e90fc3c50f9d5832ffc78b267fc61dc91d554
HadiAnik <hadiuzzamananik@gmail.com>
2025-06-29 03:37:28 +0600

    complete footer

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Navbar from "../share/Navbar";
 
 export default function HeroSection() {
@@ -16,12 +17,12 @@ export default function HeroSection() {
             </p>
             <div className="mt-8 flex justify-center">
               <div className="inline-flex rounded-md shadow">
-                <a
+                <Link
                   href="/discover"
                   className="button inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-teal-400 hover:bg-teal-500"
                 >
                   Get started
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -29,7 +30,7 @@ export default function HeroSection() {
       </div>
       <div className="  main-container lg:-mt-2 md:mt-6  ">
         <div className="w-full h-full p-4 bg-white shadow  rounded-2xl pb-8">
-          <form action="" className="">
+          <form className="">
             <div className="flex flex-col gap-2 pb-3 border-b border-slate-200/90">
               <label htmlFor="my_location" className="text-slate-700/40">
                 Your Location
@@ -53,17 +54,17 @@ export default function HeroSection() {
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    strokeLinecap="square"
+                    strokeWidth="1.5"
                     stroke="currentColor"
                     className="size-5 text-slate-700 absolute left-4"
                   >
                     <path
-                      stroke-linecap="round"
+                      strokeLinecap="round"
                       strokeLinejoin="round"
                       d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
                     />
                     <path
-                      stroke-linecap="round"
+                      strokeLinecap="round"
                       strokeLinejoin="round"
                       d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
                     />
@@ -87,14 +88,14 @@ export default function HeroSection() {
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    strokeLinecap="square"
+                    strokeWidth="1.5"
                     stroke="currentColor"
                     className="size-5 text-slate-700 absolute left-4"
                   >
                     <path
-                      stroke-linecap="round"
+                      strokeLinecap="round"
                       strokeLinejoin="round"
-                      d="M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5m.002-2.25h.005v.006H12v-.006Zm-.001 4.5h.006v.006h-.006v-.005Zm-2.25.001h.005v.006H9.75v-.006Zm-2.25 0h.005v.005h-.006v-.005Zm6.75-2.247h.005v.005h-.005v-.005Zm0 2.247h.006v.006h-.006v-.006Zm2.25-2.248h.006V15H16.5v-.005Z"
+                      d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
                     />
                   </svg>
 
@@ -116,14 +117,14 @@ export default function HeroSection() {
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    strokeLinecap="square"
+                    strokeWidth="1.5"
                     stroke="currentColor"
                     className="size-5 text-slate-700 absolute left-4"
                   >
                     <path
-                      stroke-linecap="round"
+                      strokeLinecap="round"
                       strokeLinejoin="round"
-                      d="M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5m.002-2.25h.005v.006H12v-.006Zm-.001 4.5h.006v.006h-.006v-.005Zm-2.25.001h.005v.006H9.75v-.006Zm-2.25 0h.005v.005h-.006v-.005Zm6.75-2.247h.005v.005h-.005v-.005Zm0 2.247h.006v.006h-.006v-.006Zm2.25-2.248h.006V15H16.5v-.005Z"
+                      d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
                     />
                   </svg>
 
@@ -145,12 +146,12 @@ export default function HeroSection() {
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    strokeLinecap="square"
+                    strokeWidth="1.5"
                     stroke="currentColor"
                     className="size-5 text-slate-700 absolute left-4"
                   >
                     <path
-                      stroke-linecap="round"
+                      strokeLinecap="round"
                       strokeLinejoin="round"
                       d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
                     />
--- src/components/share/Footer/FooterAddress.tsx
+++ src/components/share/Footer/FooterAddress.tsx
@@ -1,3 +1,62 @@
+import { Facebook, Globe, Instagram, Linkedin } from "lucide-react";
+import Link from "next/link";
+
 export default function FooterAddress() {
-  return <div>FooterAddress</div>;
+  return (
+    <div className="main-container w-full grid grid-cols-7 gap-4 pb-10">
+      <div className="col-span-2">
+        <h1 className="text-2xl font-semibold text-slate-700">Address</h1>
+        <div className="flex  gap-2 mt-5 text-xs text-slate-400">
+          <Facebook />
+          <Globe />
+          <Linkedin />
+          <Instagram />
+        </div>
+      </div>
+      <div className="col-span-3 flex justify-between ">
+        <div>
+          <h1 className="text-2xl font-semibold text-slate-700">About</h1>
+          <div className="flex flex-col gap-2 mt-5 text-xs text-slate-400">
+            <Link href="/about-us">About Us</Link>
+            <Link href="/destination">Destination</Link>
+            <Link href="#">News & Events</Link>
+            <Link href="#">Testimonials</Link>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-2xl font-semibold text-slate-700">Features</h1>
+          <div className="flex flex-col gap-2 mt-5 text-xs text-slate-400">
+            <Link href="#">Payments</Link>
+            <Link href="#">Vistula Accounts</Link>
+            <Link href="#">Referel Bonus</Link>
+            <Link href="#">Go-Pay</Link>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-2xl font-semibold text-slate-700">Company</h1>
+          <div className="flex flex-col gap-2 mt-5 text-xs text-slate-400">
+            <Link href="#">Carees</Link>
+            <Link href="#">Privacy & Policy</Link>
+            <Link href="#">FAQ</Link>
+            <Link href="#">Partners</Link>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-2">
+        <h1 className="text-2xl font-semibold text-slate-700">Contact Us</h1>
+        <a
+          className="text-xs text-slate-400 mt-3"
+          href="mailto:example@gmail.com"
+        >
+          example@gmail.com
+        </a>
+        <h2 className="text-xl font-semibold text-slate-700 mt-1">
+          Get the App
+        </h2>
+        <button className="mt-2 bg-red-500 py-1.5 px-8 text-sm text-white rounded-full">
+          Download App
+        </button>
+      </div>
+    </div>
+  );
 }
